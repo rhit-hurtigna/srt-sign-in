f2a1f08567c8b01991f63e291a5eeadff6865b6b
rhit-hurtigna <hurtigna@rose-hulman.edu>
2023-01-31 14:39:52 -0500

    initial commit

---
+++ admin.js
@@ -0,0 +1,81 @@
+function submitForm() {
+    answers = new Object();
+    document.querySelector('form').querySelectorAll('input, select').forEach((i) => {
+        if (i.type == 'submit') {
+            return;
+        }
+        answers[i.name] = i.value;
+        if (savedCats.includes(i.name)) {
+            if (i.value.length > 0) {
+                window.localStorage.setItem(i.name, i.value);
+            }
+        } else {
+            i.value = '';
+        }
+    });
+    db.collection("sign-ins").add(answers)
+    .then((docRef) => {
+        console.log("Document written with ID: ", docRef.id);
+    })
+    .catch((error) => {
+        console.error("Error adding document: ", error);
+    });
+}
+
+const db = firebase.firestore();
+const auth = firebase.auth();
+const provider = new firebase.auth.GoogleAuthProvider();
+
+firebase.auth().onAuthStateChanged((user) => {
+    if (user) {
+      // User is signed in, see docs for a list of available properties
+      // https://firebase.google.com/docs/reference/js/firebase.User
+    //   var uid = user.uid;
+    //   console.log(uid);
+      // ...
+    } else {
+        // console.log("nope, you not sign in...")
+        firebase.auth().signInWithRedirect(provider);
+    }
+  });
+
+function downloadAsFile(filename, text) {
+var element = document.createElement('a');
+element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+element.setAttribute('download', filename);
+
+element.style.display = 'none';
+document.body.appendChild(element);
+
+element.click();
+
+document.body.removeChild(element);
+}
+
+
+function downloadStuff() {
+    days = document.querySelector('#numDays').value
+    console.log('days :>> ', days);
+    startTime = new Date();
+    startTime.setDate(startTime.getDate() - days);
+    text = "Name,Course,Professor,SRT,Format,Hours\n"
+    categories = ['name', 'course', 'prof', 'srt', 'format', 'hours']
+    console.log('startTime :>> ', startTime);
+    db.collection("sign-ins").where('timestamp', '>', startTime).orderBy('course').orderBy('prof').orderBy('name').orderBy('srt')
+    .get()
+    .then((querySnapshot) => {
+        querySnapshot.forEach((doc) => {
+            dat = doc.data()
+            categories.forEach((cat) => {
+                text = text + `${dat[cat]},`
+            });
+            text = text.substring(0, text.length-1) + '\n';
+        });
+        downloadAsFile(`SRTsignins_${(new Date().toLocaleDateString('en-US'))}.csv`, text);
+        document.querySelector('#error').textContent = ''
+    })
+    .catch((error) => {
+        document.querySelector('#error').textContent = `Error getting documents: ${error}`
+        console.log("Error getting documents: ", error);
+    });
+}
---
+++ index.js
@@ -0,0 +1,88 @@
+function HSVtoRGB(h, s, v) {
+    var r, g, b, i, f, p, q, t;
+    if (arguments.length === 1) {
+        s = h.s, v = h.v, h = h.h;
+    }
+    i = Math.floor(h * 6);
+    f = h * 6 - i;
+    p = v * (1 - s);
+    q = v * (1 - f * s);
+    t = v * (1 - (1 - f) * s);
+    switch (i % 6) {
+        case 0: r = v, g = t, b = p; break;
+        case 1: r = q, g = v, b = p; break;
+        case 2: r = p, g = v, b = t; break;
+        case 3: r = p, g = q, b = v; break;
+        case 4: r = t, g = p, b = v; break;
+        case 5: r = v, g = p, b = q; break;
+    }
+    r = Math.floor(r*255);
+    g = Math.floor(g*255);
+    b = Math.floor(b*255);
+    return `#${('00' + r.toString(16)).substr(-2)}${('00' + g.toString(16)).substr(-2)}${('00' + b.toString(16)).substr(-2)}`;
+}
+
+const savedCats = ['name', 'course', 'prof', 'srt', 'format']
+let catVal;
+savedCats.forEach((c) => {
+    catVal = window.localStorage.getItem(c);
+    if (catVal) {
+        document.querySelector(`#${c}`).value = catVal;
+    }
+});
+
+function submitForm() {
+    answers = new Object();
+    document.querySelector('form').querySelectorAll('input, select').forEach((i) => {
+        if (i.type == 'submit') {
+            return;
+        }
+        answers[i.name] = i.value;
+        answers['timestamp'] = firebase.firestore.Timestamp.now();
+        if (savedCats.includes(i.name)) {
+            if (i.value.length > 0) {
+                window.localStorage.setItem(i.name, i.value);
+            }
+        } else {
+            i.value = '';
+        }
+    });
+    db.collection("sign-ins").add(answers)
+    .then((docRef) => {
+        document.querySelector('#error').textContent = "";
+        document.querySelector('#ty').innerHTML = 'Thanks for submitting!';
+        document.querySelector('input[type=submit]').value = 'Thank you!'
+        const now = new Date();
+        const h = ((((now.getMonth()) * 31 + now.getDate()) / 5.37) % 1)*.8 + .1;
+        const specialColor = HSVtoRGB(h, 0.2, 1);
+    
+        document.querySelector('body').style.backgroundColor = specialColor;
+        setTimeout(() => {
+            document.querySelector('body').style.backgroundColor = '#ffffff';
+            document.querySelector('#ty').innerHTML = 'Hello! Sign in here.';
+            document.querySelector('input[type=submit]').value = 'Submit'
+        }, 3000);
+    })
+    .catch((error) => {
+        console.error("Error adding document: ", error);
+        document.querySelector('#error').textContent = `Error submitting. Please tell your SRT to get Nat to fix it. ${error}`
+    });
+}
+
+// import firebase from "firebase/app";
+// import "firebase/firestore";
+
+// TODO: Replace the following with your app's Firebase project configuration
+// See: https://support.google.com/firebase/answer/7015592
+// const firebaseConfig = {
+//     FIREBASE_CONFIGURATION
+// };
+
+// Initialize Firebase
+// firebase.initializeApp(firebaseConfig);
+
+
+// Initialize Cloud Firestore and get a reference to the service
+// Your web app's Firebase configuration
+
+const db = firebase.firestore();
